e75f632fe665398a747869aa7c33997e995f71c3
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-23 16:03:06 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,3 +1,25 @@
 #include "sort.h"
+/**
+ * bubble_sort - sorts an array using bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+    size_t swapped, temp, j;
 
-void bubble_sort(int *array, size_t size);
+    while (1){
+        swapped = 0;
+        for (j = 0; j < size; j++){
+            if (array[j - 1] > array[j]){
+                temp = array[j - 1];
+                array[j - 1] = array[j];
+                array[j] = temp;
+                print_array(array, size);
+                swapped = 1;
+            }
+        }
+    if (!swapped)
+        break;
+    }
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,6 +15,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size)
-void print_list(const listint_t *list)
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 #endif
