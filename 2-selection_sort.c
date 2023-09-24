16e21f7af3f92ab202cf5af110d28e5ac541b5c9
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-09-24 13:13:30 +0300

    selection sort push

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,3 +1,27 @@
 #include "sort.h"
+/**
+ * selection_sort - sorts an array using selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
-{}
+{
+	size_t i, j, min, temp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
